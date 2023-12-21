9f8ff506fa9cb3f1c6500e0e163b5815179ba7df
Jurre de Ruiter <github@jurre.me>
2023-12-21 01:12:09 +0100

    Added more parameters + rounding and more pixel space downwards

--- ACRL.py
+++ ACRL.py
@@ -30,6 +30,11 @@ l_worldpos = 0
 l_pitch = 0
 l_roll = 0
 l_disttraveled = 0
+l_icurrenttime = 0
+l_position = 0
+
+# Rounding decimals
+DECIMALS = 3
 
 
 def acMain(ac_version):
@@ -43,55 +48,68 @@ def acMain(ac_version):
 
     # Background
     ac.setBackgroundOpacity(APP_WINDOW, 1)
-    ac.setBackgroundColor(APP_WINDOW, 255, 255, 255)
+    ac.setBackgroundColor(APP_WINDOW, 0, 0, 0)
 
     # Create the labels
-    global l_speedkmh, l_laptime, l_normsplinepos, l_velocity, l_worldpos, l_pitch, l_roll, l_disttraveled
+    global l_speedkmh, l_laptime, l_normsplinepos, l_velocity, l_worldpos, l_pitch, l_roll, l_disttraveled, l_icurrenttime, l_position
 
     l_speedkmh = ac.addLabel(APP_WINDOW, "Speed (km/h): 0")
-    ac.setPosition(l_speedkmh, 10, 30)
+    ac.setPosition(l_speedkmh, 10, 40)
 
     l_laptime = ac.addLabel(APP_WINDOW, "Lap Time: 0")
-    ac.setPosition(l_laptime, 10, 60)
+    ac.setPosition(l_laptime, 10, 70)
 
     l_normsplinepos = ac.addLabel(APP_WINDOW, "Normalized Spline Position: 0")
-    ac.setPosition(l_normsplinepos, 10, 90)
+    ac.setPosition(l_normsplinepos, 10, 100)
 
     l_velocity = ac.addLabel(APP_WINDOW, "Velocity: 0")
-    ac.setPosition(l_velocity, 10, 120)
+    ac.setPosition(l_velocity, 10, 130)
 
     l_worldpos = ac.addLabel(APP_WINDOW, "World Position: 0")
-    ac.setPosition(l_worldpos, 10, 150)
+    ac.setPosition(l_worldpos, 10, 160)
 
     l_pitch = ac.addLabel(APP_WINDOW, "Pitch: 0")
-    ac.setPosition(l_pitch, 10, 180)
+    ac.setPosition(l_pitch, 10, 190)
 
     l_roll = ac.addLabel(APP_WINDOW, "Roll: 0")
-    ac.setPosition(l_roll, 10, 210)
+    ac.setPosition(l_roll, 10, 220)
 
     l_disttraveled = ac.addLabel(APP_WINDOW, "Distance Traveled: 0")
-    ac.setPosition(l_disttraveled, 10, 240)
+    ac.setPosition(l_disttraveled, 10, 250)
+
+    l_icurrenttime = ac.addLabel(APP_WINDOW, "iCurrentTime: 0")
+    ac.setPosition(l_icurrenttime, 10, 280)
+
+    l_position = ac.addLabel(APP_WINDOW, "position: 0")
+    ac.setPosition(l_position, 10, 310)
 
     ac.console("[ACRL] Initialized")
     return APP_NAME
 
 
 def acUpdate(deltaT):
+    global l_speedkmh, l_laptime, l_normsplinepos, l_velocity, l_worldpos, l_pitch, l_roll, l_disttraveled, l_icurrenttime, l_position
+
     # Update the labels
     ac.setText(
-        l_speedkmh, "Speed (km/h): {}".format(ac.getCarState(0, acsys.CS.SpeedKMH)))
+        l_speedkmh, "Speed (km/h): {}".format(ac.getCarState(0, round(acsys.CS.SpeedKMH, DECIMALS))))
     ac.setText(l_laptime, "Lap Time: {}".format(
         ac.getCarState(0, acsys.CS.LapTime)))
     ac.setText(l_normsplinepos, "Normalized Spline Position: {}".format(
-        ac.getCarState(0, acsys.CS.NormalizedSplinePosition)))
+        ac.getCarState(0, round(acsys.CS.NormalizedSplinePosition, DECIMALS))))
     ac.setText(l_velocity, "Velocity: {}".format(
         ac.getCarState(0, acsys.CS.Velocity)))
     ac.setText(l_worldpos, "World Position: {}".format(
         ac.getCarState(0, acsys.CS.WorldPosition)))
-    ac.setText(l_pitch, "Pitch: {}".format(info.physics.pitch))
-    ac.setText(l_roll, "Roll: {}".format(info.physics.roll))
+    ac.setText(l_pitch, "Pitch: {}".format(
+        round(info.physics.pitch, DECIMALS)))
+    ac.setText(l_roll, "Roll: {}".format(round(info.physics.roll, DECIMALS)))
     ac.setText(l_disttraveled, "Distance Traveled: {}".format(
-        info.physics.distanceTraveled))
+        info.graphics.distanceTraveled))
+    ac.setText(l_icurrenttime, "iCurrentTime: {}".format(
+        info.graphics.iCurrentTime))
+    ac.setText(l_position, "position: {}".format(
+        info.graphics.position))
 
 
 def acShutdown():
