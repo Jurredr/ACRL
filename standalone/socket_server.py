6cd80f0191eefdbf258b664eac3f716ad676f3a0
Jurre de Ruiter <github@jurre.me>
2024-01-04 11:35:20 +0100

    Added screengrab and basic socket example

---
+++ standalone/socket_server.py
@@ -0,0 +1,22 @@
+import socket
+import time
+import struct
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            current_time = time.time()
+            print(current_time)
+            # current time to bytes
+            data = struct.pack('!d', current_time)
+            conn.sendall(data)
