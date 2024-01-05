1adcd0c036c58e86671daf7e1376a2bb4100b713
Jurre de Ruiter <github@jurre.me>
2024-01-05 22:41:48 +0100

    Added documentation for learning_curve function

--- standalone/graphs.py
+++ standalone/graphs.py
@@ -3,6 +3,13 @@ import matplotlib.pyplot as plt
 
 
 def plot_learning_curve(n_games, scores, figure_file):
+    """
+    Plot the learning curve of the agent.
+
+    :param n_games: The number of games played.
+    :param scores: The scores of the games.
+    :param figure_file: The file to save the figure to.
+    """
     x = [i+1 for i in range(n_games)]
     running_avg = np.zeros(len(scores))
     for i in range(len(running_avg)):
